65c7751f9a55146ecb52e67586fc4606f21565f8
oliver <swoliverro@lewisu.edu>
2020-11-07 13:34:40 -0600

    test

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html')); 
   //__dirname : It will resolve to your project folder. 
 }); 
-//app.listen(3000); 
-//console.log("Server running at Port 3000");
+
 
 
 
